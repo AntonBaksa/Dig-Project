b003664b2624fb338b1aa4a02e69c1bc09da94ba
GamingAlexander <alexander.johansson@elev.ga.lbs.se>
2025-03-24 15:39:43 +0100

    PSUUUSHS

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -1,18 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator; // Referens till Animator
+    public Transform attackPoint; // Punkt där attacken träffar
+    public float attackRange = 0.5f; // Radie för attacken
+    public LayerMask enemyLayers; // Fiendens LayerMask
+    public int attackDamage = 20; // Skada på fiender
 
-    // Update is called once per frame
+    public float attackCooldown = 0.5f; // Tid mellan attacker
+    private bool canAttack = true; // Kontroll för cooldown
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && canAttack)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        canAttack = false;
+
+        // Spela attack-animationen
+        animator.SetTrigger("Attack");
+
+        // Vänta lite så att animationen hinner träffa
+        yield return new WaitForSeconds(0.2f);
+
+        // Kolla om fiender träffas
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+        }
+
+        // Vänta innan spelaren kan attackera igen
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,41 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Movement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    public int maxHealth = 100;
+    public int attackDamage = 20;
+    public float attackRange = 1f;
+    public Transform attackPointRight;
+    public Transform attackPointLeft;
+    public Transform attackPointUp;
+    public Transform attackPointDown;
+    public LayerMask opponentLayer;
+    public Image healthBar;
 
-    public float jumpForce;
-    
+    private int currentHealth;
+    private Rigidbody2D rb;
+    private bool isFacingRight = true;
 
-    Rigidbody2D rb;
+    [SerializeField] KeyCode jumpKey;
+    [SerializeField] KeyCode leftKey;
+    [SerializeField] KeyCode rightKey;
+    [SerializeField] KeyCode attackKey;
+    [SerializeField] KeyCode downKey;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        HandleMovement();
+        HandleAttack();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(leftKey))
         {
-            transform.position += new Vector3(-5, 0, 0) * speed * Time.deltaTime;
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            isFacingRight = false;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(rightKey))
         {
-            transform.position -= new Vector3(-5, 0, 0) * speed * Time.deltaTime;
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            isFacingRight = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        if (Input.GetKeyDown(jumpKey) && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
 
-        
+    private void HandleAttack()
+    {
+        if (Input.GetKeyDown(attackKey))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Transform attackPoint = GetActiveAttackPoint();
+        if (attackPoint == null) return;
+
+        Collider2D[] hitOpponents = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, opponentLayer);
+        foreach (Collider2D opponent in hitOpponents)
+        {
+            PlayerController opponentScript = opponent.GetComponent<PlayerController>();
+            if (opponentScript != null)
+            {
+                opponentScript.TakeDamage(attackDamage);
+            }
+        }
+    }
+
+    private Transform GetActiveAttackPoint()
+    {
+        if (Input.GetKey(downKey)) return attackPointDown;
+        if (!IsGrounded()) return attackPointUp;
+        return isFacingRight ? attackPointRight : attackPointLeft;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        UpdateHealthBar();
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log(gameObject.name + " has been defeated!");
+        gameObject.SetActive(false); // Deactivate the player on death
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.fillAmount = (float)currentHealth / maxHealth;
+        }
     }
 
     private bool IsGrounded()
@@ -43,5 +120,12 @@ public class Movement : MonoBehaviour
         return Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
     }
 
-
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        if (attackPointRight != null) Gizmos.DrawWireSphere(attackPointRight.position, attackRange);
+        if (attackPointLeft != null) Gizmos.DrawWireSphere(attackPointLeft.position, attackRange);
+        if (attackPointUp != null) Gizmos.DrawWireSphere(attackPointUp.position, attackRange);
+        if (attackPointDown != null) Gizmos.DrawWireSphere(attackPointDown.position, attackRange);
+    }
 }
